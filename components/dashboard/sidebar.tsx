0f634d52f3ab53f5d0cde19bfcfcf73242f337fb
Humboorgir <hardcraftap@gmail.com>
2023-11-19 17:31:26 +0330

    perf(hook): optimize useServer by removing SWR
    I'm not sure why exactly but for some reason it took around 4-5 seconds to fetch the data with SWR and
    around half a second (probably less) with plain axios, maybe I was using it wrong? doesnt matter anyways

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -1,14 +1,13 @@
 import Server from "@/components/dashboard/server";
 import useServers from "@/hooks/useServers";
-
 const Sidebar = () => {
-  const servers = useServers();
-  if (!servers || !servers.data) return <div>Loading servers</div>;
+  const { servers, loading, error } = useServers();
+  if (!servers) return <div>Loading</div>;
 
   return (
     <ul className="h-screen bg-black overflow-y-scroll px-3 py-2">
-      {servers.data.map((server) => {
-        return <Server server={server} isLoading={servers.isLoading} />;
+      {servers.map((server, i) => {
+        return <Server key={i} server={server} isLoading={loading} />;
       })}
     </ul>
   );
