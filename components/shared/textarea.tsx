b2b6060d63679c71faed00563ffc7ad0d4f5c80a
Humboorgir <hardcraftap@gmail.com>
2023-11-26 19:08:23 +0330

    feat(component): update TextArea border and text color

--- components/shared/textarea.tsx
+++ components/shared/textarea.tsx
@@ -8,12 +8,12 @@ const TextArea = React.forwardRef<HTMLTextAreaElement, InputProps>(
   ({ className, placeholder, disabled, maxLength = 140, ...props }, ref) => {
     console.log(disabled);
     return (
-      <div className={cn("relative min-h-[120px] min-w-[400px] max-w-xl", className)}>
+      <div className={cn("relative min-h-[120px] min-w-[700px] max-w-[80vw]", className)}>
         <textarea
           className="absolute flex top-0 left-0 bottom-0 right-0 rounded-md bg-background px-3 py-3
         ring-offset-background focus-visible:outline-none border disabled:cursor-not-allowed 
-        disabled:opacity-50 ring-offset-3border-3 border-secondary transition-[border]
-         placeholder:text-transparent peer resize-none"
+        disabled:opacity-50 ring-offset-3border-3 border-secondary/60 transition-[border]
+         placeholder:text-transparent peer resize-none focus:border-secondary"
           maxLength={maxLength}
           placeholder={placeholder}
           disabled={disabled}
@@ -23,9 +23,9 @@ const TextArea = React.forwardRef<HTMLTextAreaElement, InputProps>(
         <label
           className="absolute peer-placeholder-shown:top-2
           peer-placeholder-shown:left-2 peer-placeholder-shown:text-base
-          peer-focus:!-top-1.5 peer-focus:!left-[8px] peer-focus:!text-xs
+          peer-focus:!-top-1.5 peer-focus:!left-[8px] peer-focus:!text-xs peer-focus:!text-secondary
            bg-background px-1 transition-all duration-[150ms] pointer-events-none
-        z-10 top-0 left-[8px] text-secondary peer-disabled:opacity-50 text-xs">
+        z-10 -top-1.5 left-[8px] text-secondary/60 peer-disabled:opacity-50 text-xs">
           {placeholder}
         </label>
       </div>
