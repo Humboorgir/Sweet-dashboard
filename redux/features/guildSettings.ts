73d234822e95ea464d4dd9a4a03c83bc6d5efa33
Humboorgir <hardcraftap@gmail.com>
2023-12-16 20:56:51 +0330

    feat(redux): add welcome & goodbye channel to guildSettings

--- redux/features/guildSettings.ts
+++ redux/features/guildSettings.ts
@@ -1,17 +1,26 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+type Channel = {
+  title: string;
+  value: string;
+};
+
 type State = {
   welcomeMsgsEnabled: boolean;
   welcomeMsg: string;
+  welcomeChannel: Channel;
   goodbyeMsgsEnabled: boolean;
   goodbyeMsg: string;
+  goodbyeChannel: Channel;
 };
 
 const initialState = {
   welcomeMsgsEnabled: false,
   welcomeMsg: "",
+  welcomeChannel: { title: "", value: "" },
   goodbyeMsgsEnabled: false,
   goodbyeMsg: "",
+  goodbyeChannel: { title: "", value: "" },
 } as State;
 
 export const guildSettings = createSlice({
@@ -30,15 +39,30 @@ export const guildSettings = createSlice({
     setGoodbyeMsg: (state, action: PayloadAction<string>) => {
       state.goodbyeMsg = action.payload;
     },
+    setWelcomeChannel: (state, action: PayloadAction<Channel>) => {
+      state.welcomeChannel = action.payload;
+    },
+    setGoodbyeChannel: (state, action: PayloadAction<Channel>) => {
+      state.goodbyeChannel = action.payload;
+    },
     resetSettings: (state) => {
       state.welcomeMsgsEnabled = false;
       state.welcomeMsg = "";
+      state.welcomeChannel = { title: "", value: "" };
       state.goodbyeMsgsEnabled = false;
       state.goodbyeMsg = "";
+      state.goodbyeChannel = { title: "", value: "" };
     },
   },
 });
 
-export const { toggleWelcomeMsgs, toggleGoodbyeMsgs, setWelcomeMsg, setGoodbyeMsg, resetSettings } =
-  guildSettings.actions;
+export const {
+  toggleWelcomeMsgs,
+  toggleGoodbyeMsgs,
+  setWelcomeMsg,
+  setGoodbyeMsg,
+  resetSettings,
+  setWelcomeChannel,
+  setGoodbyeChannel,
+} = guildSettings.actions;
 export default guildSettings.reducer;
