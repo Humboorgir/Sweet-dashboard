c338a217d111ea8562e5bda0293802beae2934c5
Humboorgir <hardcraftap@gmail.com>
2023-12-24 12:19:07 +0330

    feat(SaveChanges): detect changes in automod settings

--- components/dashboard/saveChanges.tsx
+++ components/dashboard/saveChanges.tsx
@@ -3,8 +3,10 @@ import Button from "@/components/shared/button";
 import { cn } from "@/lib/utils";
 import { RootState } from "@/redux/store";
 import { useDispatch, useSelector } from "react-redux";
-import { initialState } from "@/redux/features/guildSettings";
-import { resetSettings } from "@/redux/features/guildSettings";
+import { initialState as guildSettingsInitial } from "@/redux/features/guildSettings";
+import { resetSettings as resetGuildSettings } from "@/redux/features/guildSettings";
+import { initialState as automodSettingsInitial } from "@/redux/features/automodSettings";
+import { resetAutomodSettings } from "@/redux/features/automodSettings";
 import { setAlert } from "@/redux/features/alert";
 
 import axios from "axios";
@@ -13,24 +15,37 @@ import { useEffect, useState } from "react";
 const SaveChangesButton = () => {
   const [modified, setModified] = useState(false);
   const guildSettings = useSelector((state: RootState) => state.guildSettings);
+  const automodSettings = useSelector((state: RootState) => state.automodSettings);
   const guild = useSelector((state: RootState) => state.guild);
 
+  // detecting changes in guildSettings (i'll rename it to welcomerSettings later)
   useEffect(() => {
     let guildSettingsString = JSON.stringify(guildSettings);
-    let initialStateString = JSON.stringify(initialState);
+    let initialStateString = JSON.stringify(guildSettingsInitial);
     if (guildSettingsString != initialStateString) setModified(true);
     else setModified(false);
   }, [guildSettings]);
 
+  // detecting changes in automodSettings
+  useEffect(() => {
+    let automodSettingsString = JSON.stringify(automodSettings);
+    let initialStateString = JSON.stringify(automodSettingsInitial);
+    if (automodSettingsString != initialStateString) setModified(true);
+    else setModified(false);
+  }, [automodSettings]);
+
   const dispatch = useDispatch();
 
-  function resetGuildSettings() {
-    dispatch(resetSettings());
+  function resetSettings() {
+    dispatch(resetGuildSettings());
+    dispatch(resetAutomodSettings());
   }
 
   function updateGuildSettings() {
     // for debugging purposes
     if (!guild.id) return console.log(`[UpdateGuildSettings] No guild.id was found`);
+
+    // TODO: make this work with automod settings
     axios
       .put(`/api/guild/${guild.id}`, guildSettings)
       .then((res) => res.data)
@@ -52,7 +67,7 @@ const SaveChangesButton = () => {
       <h3 className="text-lg mb-4 md:mb-0">Careful — you have unsaved changes!</h3>
       <div>
         <Button
-          onClick={resetGuildSettings}
+          onClick={resetSettings}
           className="text-secondary mr-2 md:text-lg"
           rippleColor="#7C72FF"
           variant="link"
