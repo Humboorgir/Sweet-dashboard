79121599002035f8423735413a1f5166b0087809
Humboorgir <hardcraftap@gmail.com>
2023-12-24 12:10:59 +0330

    feat(Select): add utility props to make it more reusable

--- components/shared/select.tsx
+++ components/shared/select.tsx
@@ -1,20 +1,31 @@
 import Button from "@/components/shared/button";
 
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { HTMLAttributes, useState } from "react";
 
 type Option = {
   title: string;
   value: string;
 };
 
-type Props = {
+type Props = React.HTMLProps<HTMLDivElement> & {
   children: React.ReactNode;
   options: Option[];
+  btnClassName?: string;
+  menuClassName?: string;
+  menuItemClassName?: string;
   onChange?: (option: Option) => void;
 };
 
-const Select = ({ children, options, onChange = () => {} }: Props) => {
+const Select = ({
+  children,
+  options,
+  btnClassName,
+  menuClassName,
+  menuItemClassName,
+  className,
+  onChange = () => {},
+}: Props) => {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState<Option>({ title: "", value: "" });
 
@@ -30,10 +41,11 @@ const Select = ({ children, options, onChange = () => {} }: Props) => {
 
   // select component
   return (
-    <div className="relative w-fit h-fit">
+    <div className={cn("relative w-fit h-fit", className)}>
       <Button
         className={cn(
-          "border-secondary text-secondary min-w-[200px] transition-all",
+          "border-secondary text-secondary min-w-[200px] transition-all w-full",
+          btnClassName,
           open && "rounded-b-none"
         )}
         rippleColor="#7C72FF"
@@ -48,14 +60,18 @@ const Select = ({ children, options, onChange = () => {} }: Props) => {
           `absolute invisible top-full left-[50%] translate-x-[-50%] w-full min-w-[200px] bg-neutral-900 z-20 scale-[.8]
            opacity-0 transition-all duration-100 origin-top max-h-[200px] overflow-y-scroll delay-100 rounded-b-md
            border-b border-b-secondary`,
+          menuClassName,
           open && "scale-100 opacity-100 visible"
         )}>
         {options &&
           options.map((option, i) => (
             <Button
               key={i}
-              className="w-full justify-start text-secondary border-secondary border-t-0 first-of-type:border-t rounded-none
-            last-of-type:rounded-b-md"
+              className={cn(
+                `w-full justify-start text-secondary border-secondary border-t-0 first-of-type:border-t rounded-none
+            last-of-type:rounded-b-md`,
+                menuItemClassName
+              )}
               rippleColor="#7C72FF"
               variant="outline"
               onClick={() => selectOption(option)}>
