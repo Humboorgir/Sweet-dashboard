8fb5e4e6fcd0795e374f6d274d68228124d386cf
Humboorgir <hardcraftap@gmail.com>
2024-01-28 21:01:56 +0330

    feat(Modal): rewrite animation with framer-motion

--- components/shared/modal.tsx
+++ components/shared/modal.tsx
@@ -1,4 +1,4 @@
-import Backdrop from "@/components/shared/backdrop";
+import { motion, cubicBezier, AnimatePresence } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
@@ -11,19 +11,58 @@ type Props = {
 
 const Modal = ({ children, open, handleClose, className, ...props }: Props) => {
   return (
-    <Backdrop open={open} handleClose={handleClose}>
-      <div
-        onClick={(e) => e.stopPropagation()}
-        {...props}
-        className={cn(
-          `bg-neutral-900 text-foreground p-6 max-w-[90vw] opacity-0 scale-[0.8] invisible ease-out delay-100
-        rounded-md flex flex-col items-center transition-all duration-200 border border-neutral-800`,
-          open && "!opacity-100 scale-100 visible",
-          className
-        )}>
-        {children}
-      </div>
-    </Backdrop>
+    <AnimatePresence>
+      {/* backdrop  */}
+      {open && (
+        <motion.div
+          key="backdrop"
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 100,
+          }}
+          exit={{ opacity: 0 }}
+          transition={{
+            duration: 0.2,
+            delay: 0.1,
+          }}
+          className="fixed left-0 right-0 top-0 bottom-0 bg-black/60 z-[100] grid place-items-center"
+          onClick={handleClose}>
+          {/* modal window  */}
+          {open && (
+            <motion.div
+              key="modal"
+              initial={{
+                opacity: 0,
+                scale: 0.88,
+              }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+              }}
+              exit={{
+                opacity: 0,
+                scale: 0.75,
+              }}
+              transition={{
+                ease: cubicBezier(0.33, 1, 0.68, 1),
+                duration: 0.2,
+                delay: 0.1,
+              }}
+              onClick={(e) => e.stopPropagation()}
+              className={cn(
+                `bg-neutral-900 text-foreground p-6 max-w-[90vw] md:max-w-[640px] w-fit !ease-out rounded-md 
+                flex flex-col border border-neutral-800`,
+                className
+              )}
+              {...props}>
+              {children}
+            </motion.div>
+          )}
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
