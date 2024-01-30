c6e2a643d74ea0c45c677196b417cbacc7c5d4b8
Humboorgir <hardcraftap@gmail.com>
2024-01-30 17:06:44 +0330

    feat(Alert): rewrite animation with framer-motion

--- components/shared/alert.tsx
+++ components/shared/alert.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { cn } from "@/lib/utils";
+import { AnimatePresence, cubicBezier, motion } from "framer-motion";
 import { useDispatch, useSelector } from "react-redux";
 import { resetAlert } from "@/redux/features/alert";
 import type { RootState } from "@/redux/store";
@@ -42,21 +43,40 @@ const Alert = () => {
 
   const title = alert.type[0].toUpperCase() + alert.type.slice(1);
 
-  if (open)
-    return (
-      <div
-        className={cn(
-          `fixed z-[100] top-5 py-1.5 px-4 rounded-full flex items-center justify-center
-    left-[50%] translate-x-[-50%] text-white animate-alertOpen`,
-          customStyle
-        )}>
-        <strong className="flex items-center mr-2">
-          {icon}
-          {title}!
-        </strong>{" "}
-        {alert.message}
-      </div>
-    );
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          initial={{
+            opacity: 88,
+            scale: 0,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+          }}
+          exit={{
+            opacity: 0.88,
+            scale: 0,
+          }}
+          transition={{
+            ease: cubicBezier(0.33, 1, 0.68, 1),
+            duration: 0.2,
+          }}
+          className={cn(
+            `fixed z-[100] top-5 py-1.5 px-4 rounded-full flex items-center justify-center
+          w-fit mx-auto left-0 right-0 text-white`,
+            customStyle
+          )}>
+          <strong className="flex items-center mr-2">
+            {icon}
+            {title}!
+          </strong>{" "}
+          {alert.message}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
   return null;
 };
 
