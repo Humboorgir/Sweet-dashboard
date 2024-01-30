6e0e793e6e9e2c99f9ee06f59a5c0877e42e6f37
Humboorgir <hardcraftap@gmail.com>
2024-01-30 16:38:01 +0330

    feat(api): move guildId/welcomer to its own route

--- pages/api/guild/[guildId]/index.ts
+++ pages/api/guild/[guildId]/index.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-import guildModel from "@/models/guild";
-import mongoConnect from "@/lib/mongoConnect";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!process.env.DISCORD_BOT_TOKEN) console.log("[WARNING] Couldn't find process.env.DISCORD_BOT_TOKEN");
+  if (!process.env.DISCORD_BOT_TOKEN)
+    console.log("[WARNING] Couldn't find process.env.DISCORD_BOT_TOKEN");
 
   const { guildId } = req.query;
   if (!guildId) return res.status(400);
@@ -26,48 +25,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       console.log(e);
       return res.status(500).json({ message: "Failed to fetch guild info" });
     }
-  }
-
-  // handling PUT requests
-  if (req.method == "PUT") {
-    // TODO: rewrite all this with zod
-    // (too lazy to write their types right now even tho it would probably take less time)
-    const { welcomeMsgsEnabled, welcomeMsg, welcomeChannel, goodbyeMsgsEnabled, goodbyeMsg, goodbyeChannel } =
-      req.body;
-    if (
-      typeof welcomeMsgsEnabled != "boolean" ||
-      !welcomeMsg ||
-      !welcomeChannel ||
-      typeof goodbyeMsgsEnabled != "boolean" ||
-      !goodbyeMsg ||
-      !goodbyeChannel
-    )
-      return res.status(400);
-
-    // db stuff
-    const filter = { id: guildId };
-    const newData = {
-      settings: {
-        welcome: {
-          enabled: welcomeMsgsEnabled,
-          channelId: welcomeChannel.value,
-          message: welcomeMsg,
-        },
-        goodbye: {
-          enabled: goodbyeMsgsEnabled,
-          channelId: goodbyeChannel.value,
-          message: goodbyeMsg,
-        },
-      },
-    };
-
-    mongoConnect();
-    guildModel // upsert makes it so that if such document doesnt exist, it creates one
-      .findOneAndUpdate(filter, newData, { upsert: true })
-      .then(() => res.status(200).json({ message: "Successfully saved guild settings" }))
-      .catch((e) => {
-        res.status(500).json({ message: "Failed to save guild settings" });
-        return console.log(e);
-      });
   }
 }
---
+++ pages/api/guild/[guildId]/welcomer.ts
@@ -0,0 +1,60 @@
+import guildModel from "@/models/guild";
+import mongoConnect from "@/lib/mongoConnect";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (!process.env.DISCORD_BOT_TOKEN)
+    console.log("[WARNING] Couldn't find process.env.DISCORD_BOT_TOKEN");
+
+  const { guildId } = req.query;
+  if (!guildId) return res.status(400);
+
+  // handling PUT requests
+  if (req.method == "PUT") {
+    // TODO: rewrite all this with zod
+    // (too lazy to write their types right now even tho it would probably take less time)
+    const {
+      welcomeMsgsEnabled,
+      welcomeMsg,
+      welcomeChannel,
+      goodbyeMsgsEnabled,
+      goodbyeMsg,
+      goodbyeChannel,
+    } = req.body;
+    if (
+      typeof welcomeMsgsEnabled != "boolean" ||
+      !welcomeMsg ||
+      !welcomeChannel ||
+      typeof goodbyeMsgsEnabled != "boolean" ||
+      !goodbyeMsg ||
+      !goodbyeChannel
+    )
+      return res.status(400);
+
+    // db stuff
+    const filter = { id: guildId };
+    const newData = {
+      settings: {
+        welcome: {
+          enabled: welcomeMsgsEnabled,
+          channelId: welcomeChannel.value,
+          message: welcomeMsg,
+        },
+        goodbye: {
+          enabled: goodbyeMsgsEnabled,
+          channelId: goodbyeChannel.value,
+          message: goodbyeMsg,
+        },
+      },
+    };
+
+    mongoConnect();
+    guildModel // upsert makes it so that if such document doesnt exist, it creates one
+      .findOneAndUpdate(filter, newData, { upsert: true })
+      .then(() => res.status(200).json({ message: "Successfully saved guild settings" }))
+      .catch((e) => {
+        res.status(500).json({ message: "Failed to save guild settings" });
+        return console.log(e);
+      });
+  }
+}
