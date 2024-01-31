adcab87df6eda0c022ac076e3f25dd37d2c2aef0
Humboorgir <hardcraftap@gmail.com>
2024-01-31 17:29:39 +0330

    fix(SaveChanges): not centered properly

--- components/dashboard/saveChangesBase.tsx
+++ components/dashboard/saveChangesBase.tsx
@@ -21,8 +21,8 @@ const SaveChangesBase = ({ modified, loading, resetSettings, updateSettings }: P
           exit={{ y: 20, opacity: 0 }}
           transition={{ ease: cubicBezier(0.33, 1, 0.68, 1), delay: 0.1, duration: 0.15 }}
           className={cn(
-            `fixed bottom-10 left-0 right-0 mx-auto flex justify-between items-center
-             bg-black z-20 lg:ml-24 p-4 rounded-md w-[85vw] max-w-[800px] flex-col
+            `fixed bottom-10 left-0 lg:left-24 right-0 mx-auto flex justify-between items-center
+             bg-black z-20 p-4 rounded-md w-[85vw] max-w-[800px] flex-col
               md:flex-row origin-top`
           )}>
           <h3 className="text-lg mb-4 md:mb-0">Careful — you have unsaved changes!</h3>
